bac27147ac18983e61384b6dc73c4ce489d8c1ac
JeanLLopes <jean.llopes@outlook.com>
2016-12-27 22:08:12 -0200

    Add project files.

---
+++ MVC5-PartialViewWithPagination.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5_PartialViewWithPagination.Web.Startup))]
+namespace MVC5_PartialViewWithPagination.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
